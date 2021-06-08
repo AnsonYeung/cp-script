27ba9b1b77d88cf0bf9cf63036b8c45ea57a08b5
Anson Yeung <s151204@tanghin.edu.hk>
2021-06-08 21:42:31 +0800

    Implement the `cps test` command

--- cmd/test.js
+++ cmd/test.js
@@ -1,3 +1,8 @@
+const pidusage = require('pidusage');
+const childProcess = require('child_process');
+const fs = require('fs-extra');
+const path = require('path');
+
 exports.command = 'test [problemDir]';
 
 exports.describe = 'Test the solution with the sample data';
@@ -9,6 +14,136 @@ exports.builder = yargs => {
     });
 };
 
-exports.handler = argv => {
+exports.handler = async argv => {
+    let problemJsonTask = fs.readJson(path.join(argv.problemDir, 'problem.json'));
+    let compiler = childProcess.spawn('g++', ['-std=c++17', '-ggdb3', '-DONLINE_JUDGE', '-Wall', '-o', 'solution.exe', 'solution.cpp'], {
+        stdio: ['ignore', 'inherit', 'inherit'],
+    });
+    compiler.on('close', async code => {
+        if (code != 0) {
+            console.log(`gcc exited with code ${code}`);
+            return;
+        }
 
+        let problemJson = await problemJsonTask;
+
+        let testArray = [];
+
+        for (let i in problemJson.tests) {
+            let filename = problemJson.input.type === 'file' ? problemJson.input.fileName : i.toString() + '.in';
+            testArray.push(fs.open(path.resolve(argv.problemDir, 'tests', i.toString(), filename), 'r').then(fd => {
+                let progEnd = new Promise(res => {
+                    let memUsage = 0;
+                    let output = '';
+                    let startTime = process.hrtime.bigint();
+                    let rte = false;
+                    let solutionProcess = childProcess.spawn(path.resolve(argv.problemDir, 'solution.exe'), {
+                        cwd: path.resolve(argv.problemDir, 'tests', i.toString()),
+                        stdio: [fd, 'pipe', 'inherit']
+                    });
+                    let memMonitor = () => {
+                        pidusage(solutionProcess.pid, (err, stats) => {
+                            if (err.code === 'ENOENT') return;
+                            if (err) throw err;
+                            memUsage = Math.max(memUsage, stat.memory);
+                        });
+                    };
+                    memMonitor();
+                    solutionProcess.on('close', code => {
+                        let endTime = process.hrtime.bigint();
+                        let time = Number(endTime - startTime) / 1000000;
+                        if (code !== 0) rte = true;
+                        if (problemJson.output.type === 'file') {
+                            res(fs.readFile(path.resolve(argv.problemDir, 'tests', i.toString(), problemJson.output.fileName), {encoding: 'utf8'}).then(output => {
+                                return {
+                                    output,
+                                    time,
+                                    memUsage,
+                                    rte
+                                };
+                            }));
+                        } else {
+                            res({
+                                output,
+                                time,
+                                memUsage,
+                                rte
+                            });
+                        }
+                    });
+                    solutionProcess.on('error', () => {
+                        rte = true;
+                    });
+                    solutionProcess.stdout.setEncoding('utf8');
+                    solutionProcess.stdout.on('data', (data) => {
+                        output += data;
+                    });
+                });
+                let tle = new Promise(res => {
+                    setTimeout(res.bind(null, {
+                        output: '',
+                        time: problemJson.timeLimit + 1000,
+                        memUsage: 0,
+                        rte: false
+                    }), problemJson.timeLimit + 1000);
+                });
+                return Promise.any([progEnd, tle]).then(result => {
+                    if (result.time > problemJson.timeLimit) {
+                        return {
+                            status: 'Time Limit Exceeded',
+                            accepted: false
+                        };
+                    } else if (result.memUsage > problemJson.memoryLimit * 1024 * 1024) {
+                        return {
+                            status: 'Memory Limit Exceeded',
+                            accepted: false
+                        };
+                    } else if (result.rte) {
+                        return {
+                            status: 'Runtime Error',
+                            accepted: false
+                        };
+                    } else if (result.output.trim() !== problemJson.tests[i].output.trim()) {
+                        return {
+                            status: 'Wrong Answer',
+                            accepted: false,
+                            output: result.output,
+                            expected: problemJson.tests[i].output
+                        }
+                    } else {
+                        return {
+                            status: 'Accepted',
+                            accepted: true
+                        }
+                    }
+                })
+            }));
+        }
+
+        let results = await Promise.all(testArray);
+
+        let accepted = 0;
+
+        for (let i in results) {
+            if (results[i].accepted) ++accepted;
+            else {
+                console.log(`Test ${i}: ${results[i].status}`);
+                if (results[i].status == 'Wrong Answer') {
+                    console.log(
+`Your Answer:
+${results[i].output}
+Jury's Answer:
+${results[i].expected}`
+                    );
+                }
+            }
+        }
+
+        if (accepted === results.length) {
+            console.log('All tests passed');
+        } else {
+            console.log(`${accepted} of ${results.length} total tests are passed`);
+        }
+
+    })
 };
--- utils/problem.js
+++ utils/problem.js
@@ -2,9 +2,17 @@ const fs = require('fs-extra');
 const path = require('path');
 
 exports.makeProblem = async function (p, problem) {
+    console.log(`Making problem "${problem.name}"`);
     await fs.ensureDir(p);
     let problemFileTask = fs.writeJSON(path.resolve(p, 'problem.json'), problem);
     let solutionFileTask = fs.copyFile(path.resolve(process.env.CP, 'templates', 'default.cpp'), path.resolve(p, 'solution.cpp'));
-    await Promise.all([problemFileTask, solutionFileTask]);
-    console.log('Created problem ', problem.name);
+    let inputFileTasks = [];
+    for (let i in problem.tests) {
+        let inputFilename = problem.input.type === 'file' ? problem.input.fileName : `${i}.in`;
+        inputFileTasks.push(fs.ensureDir(path.resolve(p, 'tests', i.toString())).then(() => {
+            fs.writeFile(path.resolve(p, 'tests', i.toString(), inputFilename), problem.tests[i].input)
+        }));
+    }
+    await Promise.all([problemFileTask, solutionFileTask, ...inputFileTasks]);
+    console.log(`Created problem "${problem.name}"`);
 };
